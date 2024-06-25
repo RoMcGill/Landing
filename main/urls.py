1f0527c294541ae2ca3a26666e719c8ea2f7bc0c
RoMcGill <ro.mcgill12@gmail.com>
2024-06-25 23:17:13 +0000

    styled home and added about us and get started page

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import home
+from .views import about
+from .views import getStarted
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('get-started/', getStarted, name='get-started'),
 ]
